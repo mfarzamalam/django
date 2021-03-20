f7228f67e4d5f20701b167d1c583c2517f1f7aaa
mfarzamalam <mfarzamalam@gmail.com>
2021-03-21 00:12:08 +0500

    admin panel and model class

--- Hospital6/room/admin.py
+++ Hospital6/room/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from room.models import Staff
 # Register your models here.
+
+@admin.register(Staff)
+
+class PrettyStaff(admin.ModelAdmin):
+    list_display = ('id','sid','sname','semail','spass')
+
+# admin.site.register(Staff, PrettyStaff)
--- Hospital6/room/models.py
+++ Hospital6/room/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    sid = models.IntegerField()
+    sname = models.CharField(max_length=100)
+    semail = models.CharField(max_length=100)
+    spass = models.CharField(max_length=100)
+
+    # def __str__(self):
+    #     return self.sname
