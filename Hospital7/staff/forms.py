70a66a5425c1a8a7b5e0d4914a06424ecc55e25f
mfarzamalam <mfarzamalam@gmail.com>
2021-03-21 22:33:02 +0500

    looping through the fields

--- Hospital7/staff/forms.py
+++ Hospital7/staff/forms.py
@@ -4,4 +4,4 @@ class RegisterStaff(forms.Form):
     name = forms.CharField(initial='your name', help_text='only 30 charaters allowed')
     email = forms.EmailField()
     password = forms.CharField()
-    address = forms.CharField()
+    address = forms.CharField(widget=forms.HiddenInput())
