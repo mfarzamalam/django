83a220b4989fdfac7e8286e932c2bf3674848679
mfarzamalam <mfarzamalam@gmail.com>
2021-03-21 21:56:43 +0500

    render form fields manually

--- Hospital7/staff/forms.py
+++ Hospital7/staff/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class RegisterStaff(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(initial='your name', help_text='only 30 charaters allowed')
     email = forms.EmailField()
     password = forms.CharField()
     address = forms.CharField()
