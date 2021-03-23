0689ef7acdb1c60b555c0b1fd31d6633775c802c
mfarzamalam <mfarzamalam@gmail.com>
2021-03-24 00:54:55 +0500

    cleaning and validating form field

--- School_2/staff/forms.py
+++ School_2/staff/forms.py
@@ -2,3 +2,26 @@ from django import forms
 
 class staffRegister(forms.Form):
     name = forms.CharField()
+    email = forms.EmailField()
+
+        # Custom validation for single field
+    # def clean_name(self):
+    #     clean_name_data = self.cleaned_data['name']
+
+    #     if len(clean_name_data) < 5:
+    #         raise forms.ValidationError("Enter more than 5 character")
+    #     else:
+    #         return clean_name_data
+
+
+        # Custom validation for whole form
+    def clean(self):
+        cleaned_data = super().clean()
+        cname = self.cleaned_data['name']
+        cemail = self.cleaned_data['email']
+
+        if len(cname) < 5:
+            raise forms.ValidationError('Name should not be less than or equal to 5')
+
+        if len(cemail) < 15:
+            raise forms.ValidationError('Email should not be less than or equal to 15')
--- School_2/staff/views.py
+++ School_2/staff/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .forms import staffRegister
 
+# Create your views here.
 def showStaffData(request):
-    showData = staffRegister()
+    if request.method == 'POST':
+        showData = staffRegister(request.POST)
+        if showData.is_valid():
+            name = showData.cleaned_data['name']
+            email = showData.cleaned_data['email']
+
+            print("Name:",name)
+            print("Email:",email)
+
+    else:
+        showData = staffRegister()
 
     return render(request, 'staff/staffRegister.html', {'form':showData})
