60758c5c6aa35d69f72e28c76f3190eb34be74cf
mfarzamalam <mfarzamalam@gmail.com>
2021-03-23 19:15:47 +0500

    getting form data with validation

--- Hospital8/staff/forms.py
+++ Hospital8/staff/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class staffRegister(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- Hospital8/staff/views.py
+++ Hospital8/staff/views.py
@@ -3,6 +3,20 @@ from .forms import staffRegister
 
 # Create your views here.
 def showStaffData(request):
-    sd = staffRegister()
+    if request.method == 'POST':
+        showData = staffRegister(request.POST)
+        if showData.is_valid():
+            name = showData.cleaned_data['name']
+            email = showData.cleaned_data['email']
+            password = showData.cleaned_data['password']
 
-    return render(request, 'staff/staffRegister.html', {'form':sd})
+            print("Name:",name)
+            print("Email:",email)
+            print("Password:",email)
+
+            showData = staffRegister()
+    else:
+        showData = staffRegister()
+        print("Empty Empty")
+
+    return render(request, 'staff/staffRegister.html', {'form':showData})
