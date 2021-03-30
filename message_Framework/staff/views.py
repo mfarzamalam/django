23d3bb282aae887eafb31cc8af0b8441d7603fa8
mfarzamalam <mfarzamalam@gmail.com>
2021-03-31 02:48:09 +0500

    used messages_framework

--- message_Framework/staff/views.py
+++ message_Framework/staff/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import staffRegister
+from django.contrib import messages
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -7,7 +9,21 @@ def register(request):
 
         if fm.is_valid():
             fm.save()
-            fm = staffRegister()
+            
+            # Use this
+            # messages.add_message(request, messages.INFO, 'Your Account has been created!')
+            # Or
+            messages.success(request, 'Your Account has been created')
+
+            messages.debug(request, 'This is first Debug')
+            print(messages.get_level(request))
+
+            # Decrease the level or error messages to 10=='Debug'
+            messages.set_level(request, messages.DEBUG)
+            
+            messages.debug(request, 'This is Second Debug')
+            print(messages.get_level(request))
+
     else:
         fm = staffRegister()
 
