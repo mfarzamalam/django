343a8036d960eb9d46afade041d392bfac42374b
mfarzamalam <mfarzamalam@gmail.com>
2021-03-30 18:28:22 +0500

    modelForm inheritance

--- CRUD_1/staff/forms.py
+++ CRUD_1/staff/forms.py
@@ -12,3 +12,10 @@ class studentRegistration(forms.ModelForm):
             'email':forms.EmailInput(attrs={'class':'form-control'}),
             'password':forms.PasswordInput(attrs={'class':'form-control'}),
         }
+
+    
+    # ModelForm inheritance
+    
+# class TeacherRegistration(studentRegistration):
+#     class Meta(studentRegistration.Meta):
+#         fields = ['teacher_name','email','password']
