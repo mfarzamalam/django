e1ac02c16d36803b27600c9acbc1b897a8728411
mfarzamalam <mfarzamalam@gmail.com>
2021-04-09 15:27:43 +0500

    Working with UserCreationForm

--- School_5/staff/views.py
+++ School_5/staff/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib import messages
 
 # Create your views here.
+def sign_up(request):
+    if request.method == "POST":
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, 'Account Created Successfully')
+   
+    else:
+        fm = SignUpForm()
+
+    return render(request, 'staff/signup.html',{'form':fm})
