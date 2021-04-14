4afb03875ff9a968e9a9a2ffcdc169db60cfab23
mfarzamalam <mfarzamalam@gmail.com>
2021-04-14 23:55:18 +0500

    session counter

--- PageCounter/counter/views.py
+++ PageCounter/counter/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
+def home(request):
+    count = request.session.get('count',0)
+    newCount = count + 1
+    request.session['count'] = newCount
+
+    return render(request, 'counter/home.html', {'count': newCount})
+
+
+def deleteSession(request):
+
+        request.session.flush()
+
+        return HttpResponseRedirect('/')
