99c394f37b82372d77ac2c8e670734ac39273dfe
mfarzamalam <mfarzamalam@gmail.com>
2021-04-15 07:06:51 +0500

    cache per-url

--- School_11/staff/views.py
+++ School_11/staff/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
 # Create your views here.
 
-@cache_page(30)
 def home(request):
     return render(request, 'staff/home.html')
 
