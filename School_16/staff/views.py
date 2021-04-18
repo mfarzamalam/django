542e0b2c28db6551442a91024a060b6af846f5c2
mfarzamalam <mfarzamalam@gmail.com>
2021-04-18 23:21:31 +0500

    initial

---
+++ School_16/staff/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+def home(request):
+        pass
+
+#     print(data)
+#     print("query =",data.query)
+
+#     return render(request, 'staff/home.html', {'students':data})
