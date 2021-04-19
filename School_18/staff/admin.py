061058939ffc4c0a4800de33982bcdf074b48e6d
mfarzamalam <mfarzamalam@gmail.com>
2021-04-20 01:33:44 +0500

    model inheritance with abstract class

--- School_18/staff/admin.py
+++ School_18/staff/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from staff.models import Student, Teacher, Contractor
 
 # Register your models here.
+@admin.register(Student)
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['id','name','age','fees']
+
+@admin.register(Teacher)
+class AdminTeacher(admin.ModelAdmin):
+    list_display = ['id','name','age','date','salary']
+
+@admin.register(Contractor)
+class AdminContractor(admin.ModelAdmin):
+    list_display = ['id','name','age','date','payment']
--- School_18/staff/models.py
+++ School_18/staff/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Common(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+
+class Student(Common):
+    fees = models.IntegerField()
+    date = None
+
+class Teacher(Common):
+    salary = models.IntegerField()
+
+class Contractor(Common):
+    date = models.DateTimeField()
+    payment = models.IntegerField()
