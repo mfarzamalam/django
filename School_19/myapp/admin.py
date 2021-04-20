cc1dc0eabb1ba3c07d77b9f58084ad683f2b00b7
mfarzamalam <mfarzamalam@gmail.com>
2021-04-21 02:42:10 +0500

    Model one to one relationship

--- School_19/myapp/admin.py
+++ School_19/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Page, Like
 
 # Register your models here.
+@admin.register(Page)
+class Page_Admin(admin.ModelAdmin):
+    list_display = ['page_name','page_category','page_publish_date','user']
+
+@admin.register(Like)
+class Like_Admin(admin.ModelAdmin):
+    list_display = ['page_name','page_category','page_publish_date','user', 'likes']
--- School_19/myapp/models.py
+++ School_19/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Page(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, limit_choices_to={'is_staff':True})
+    page_name = models.CharField(max_length=100)
+    page_category = models.CharField(max_length=100)
+    page_publish_date = models.DateField()
+
+class Like(Page):
+    panna = models.OneToOneField(Page, on_delete=models.CASCADE, primary_key=True, parent_link=True)
+    likes = models.IntegerField()
