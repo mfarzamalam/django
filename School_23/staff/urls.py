bc1ed78dff5e8b37fab43adf4b22fb99b606aab3
mfarzamalam <mfarzamalam@gmail.com>
2021-04-25 01:09:19 +0500

    authentication with url required decorators

--- School_23/staff/urls.py
+++ School_23/staff/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.admin.views.decorators import staff_member_required
+
 
 urlpatterns = [
+    # path("profile/", login_required(views.my_profile), name="profile"),
+    # path("contact/", staff_member_required(views.ContactView.as_view()), name="contact"),
+    
     path("profile/", views.my_profile, name="profile"),
-    path("about/", views.my_about, name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
+    path("about/", views.my_about.as_view(), name="about"),
 ]
--- School_23/staff/views.py
+++ School_23/staff/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
     # for staff member only user the decorator @staff_member_required
 
-@login_required
+# @login_required
+# def my_profile(request):
+#     return render(request, 'registration/profile.html')
+
+# @staff_member_required
+# def my_about(request):
+#     return render(request, 'registration/about.html')
+
+
+@method_decorator(login_required, name='dispatch')
+class ContactView(TemplateView):
+    template_name = 'registration/contact.html'
+
+    # @method_decorator(login_required)
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super().dispatch(request, *args, **kwargs)
+
+
 def my_profile(request):
     return render(request, 'registration/profile.html')
 
-@staff_member_required
-def my_about(request):
-    return render(request, 'registration/about.html')
+
+class my_about(TemplateView):
+    template_name = 'registration/about.html'
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
