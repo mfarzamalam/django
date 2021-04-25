af1b9c7f8a2a39f0d5a22b3eb757811d0fdce384
mfarzamalam <mfarzamalam@gmail.com>
2021-04-26 00:16:51 +0500

    pagination with function base

--- School_25/blog/models.py
+++ School_25/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    author = models.CharField(max_length=100)
+    publish_date = models.DateTimeField()
--- School_25/blog/views.py
+++ School_25/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
+def ShowAllPost(request):
+    all_post = Post.objects.all().order_by('id')
+    paginator = Paginator(all_post,4,orphans=2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/showPost.html', {'posts':page_obj})
