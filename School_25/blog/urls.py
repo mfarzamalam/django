7c2c7d9cb1e8b8e39b222532dfa695f131699c95
mfarzamalam <mfarzamalam@gmail.com>
2021-04-26 01:14:03 +0500

    pagination with class base

--- School_25/blog/urls.py
+++ School_25/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.ShowAllPost)
+    path('view/', views.ShowAllPost),
+    path('view2/', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>', views.PostDetalView.as_view(), name='post')
 ]
--- School_25/blog/views.py
+++ School_25/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def ShowAllPost(request):
@@ -10,3 +11,30 @@ def ShowAllPost(request):
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'blog/showPost.html', {'posts':page_obj})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    ordering = ['id']
+    paginate_by = 3
+    paginate_orphans = 2
+
+    # def get_context_data(self, *args, **kwargs):
+    #     try:
+    #         return super(PostListView, self).get_context_data(*args, **kwargs)
+    #     except:
+    #         self.kwargs['page'] = 1
+    #         return super(PostListView, self).get_context_data(*args, **kwargs)
+            
+
+    def paginate_queryset(self, queryset, page_size):
+        try:
+            return super(PostListView, self).paginate_queryset(queryset, page_size)
+        except:
+            self.kwargs['page'] = 1
+            return super(PostListView, self).paginate_queryset(queryset, page_size)
+
+class PostDetalView(DetailView):
+    model = Post
+    template_name = "blog/post.html"
