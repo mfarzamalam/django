935a60d3ac874b7adb4287b4c2d172b89c6551b9
mfarzamalam <mfarzamalam@gmail.com>
2021-04-28 02:31:47 +0500

    image uploading

--- Image_uploader/Image_uploader/settings.py
+++ Image_uploader/Image_uploader/settings.py
@@ -106,16 +106,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Karachi'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- Image_uploader/myapp/views.py
+++ Image_uploader/myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import ImageForm
+from .models import Image
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('/')
+
+    else:
+        form = ImageForm()
+
+    img = Image.objects.all()
+
+    return render(request, 'myapp/home.html', {'form':form, 'img':img})
